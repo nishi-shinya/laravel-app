a69ed227633aabd05bb24185bc9705ba2e6c19d7
georgia <georgia6954@gmail.com>
2021-01-01 02:30:11 +0900

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
-@extends('layouts.app')
+<x-home-master>
+    @section('content')
+        <h1>Home</h1>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+{{--        <div class="container">--}}
+{{--            <div class="row justify-content-center">--}}
+{{--                <div class="col-md-8">--}}
+{{--                    <div class="card">--}}
+{{--                        <div class="card-header">{{ __('Dashboard') }}</div>--}}
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+{{--                        <div class="card-body">--}}
+{{--                            @if (session('status'))--}}
+{{--                                <div class="alert alert-success" role="alert">--}}
+{{--                                    {{ session('status') }}--}}
+{{--                                </div>--}}
+{{--                            @endif--}}
+
+{{--                            You are logged in!--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+    @endsection
+</x-home-master>
